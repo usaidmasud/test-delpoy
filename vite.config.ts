9a4c44b5fa68645061eb6379be107806a93aaf50
usaidmasud <syaripmasud@gmail.com>
2024-04-07 07:30:03 +0800

    udpate

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import preact from '@preact/preset-vite'
 export default defineConfig({
   base:"/",
   plugins: [preact()],
-  // preview: {
-  //   port: 6060,
-  //   strictPort: true,
-  //  },
-  //  server: {
-  //   port: 6060,
-  //   strictPort: true,
-  //   host: true,
-  //   origin: "http://0.0.0.0:6060",
-  //  },
+  preview: {
+    port: 6060,
+    strictPort: true,
+   },
+   server: {
+    port: 6060,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:6060",
+   },
 })
